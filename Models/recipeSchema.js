05cc055880d4d4ec26cca9c1a03d8be9fc44eb30
LeoAlexThomas <leoalex960@gmail.com>
2025-11-29 15:55:30 +0530

    added recipe schema, routes and controllers

--- Models/recipeSchema.js
+++ Models/recipeSchema.js
@@ -1,1 +1,21 @@
 import mongoose from "mongoose";
+
+const recipeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+});
+
+// Creating collection
+const recipes = mongoose.model("recipes", recipeSchema);
+
+export default recipes;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./Database/dbConfig.js";
+import recipeRoutes from "./Routers/recipeRouters.js";
 
 // Dotenv configure
 dotenv.config();
@@ -17,6 +18,9 @@ app.use(cors());
 
 // Database Connection
 connectDB();
+
+// Custom Routes
+app.use("/api/recipe", recipeRoutes);
 
 // Default route
 app.get("/", (req, res) => {
